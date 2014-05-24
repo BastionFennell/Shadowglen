10f902a5adb987759edb8063e56826ced8592d97
Bastion Fennell <bastionfennell@thefrontside.net>
2014-05-24 03:28:45 -0500

    First rendition of the site

---
+++ js/app.js
@@ -0,0 +1,28 @@
+window.App = Ember.Application.create();
+
+App.Router.map(function(){
+  this.resource("front", { path: "/" });
+  this.resource("TX", {path: "/TX"});
+});
+
+App.FrontController = Ember.ObjectController.extend({
+  actions: {
+    clickState: function(state){
+      this.transitionToRoute(state.replace("US-", ""));
+    }
+  }
+});
+
+App.MapView = Ember.View.extend({
+  templateName: 'map',
+  didInsertElement: function(){
+    this._super();
+    self = this
+    $('#world-map').vectorMap({map: 'us_aea_en',
+      backgroundColor: "black",
+      onRegionClick: function(e, code) {
+        self.get("controller").send("clickState", code);
+      }
+    });
+  }
+})
